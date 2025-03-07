3de8b7058ed628567018bc4733a4ca9cfe036b35
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-03-07 12:13:46 +0000

    updated README with instructions, saved sensitive data into data.env

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # website_deployment
 day70
+
+1. "source venv/bin/activate"
+2. "source data.env" (saved FLASK_KEY and DB_URL)
+3. run "python3 main.py"
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from typing import List
 from flask import Flask, request, abort, render_template, redirect, url_for, flash
@@ -15,7 +16,7 @@ from forms import CreatePostForm, RegistrationForm, LoginForm, CommentForm
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 gravatar = Gravatar(app,
@@ -35,7 +36,7 @@ app.jinja_env.globals['gravatar'] = gravatar
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
